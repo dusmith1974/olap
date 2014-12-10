9329ca1a8869bcc55dccb0f5b5688eaec4dc3b5c
Duncan Smith <duncan.smith@telperin.com>
2014-12-10 13:00:45 +0000

    VS/gcc dual build.

---
+++ olreplay/olreplay_pch.h
@@ -0,0 +1,48 @@
+#if defined(_MSC_VER) && _MSC_VER >= 1400 
+
+//#pragma warning(disable:4512)
+//#pragma warning(disable:4996)
+//#pragma warning(disable:4510)
+//#pragma warning(disable:4610)
+//#pragma warning(disable:4100)
+
+/*#include <iostream>
+#include <algorithm>
+#include <set>
+#include <thread>
+#include <map>
+#include <utility>
+#include <vector>
+#include <memory>
+#include <string>
+#include <deque>
+#include <fstream>
+#include <sstream>
+#include <cmath>*/
+
+/*#include "boost/bind.hpp"
+#include "boost/asio.hpp"
+#include "boost/algorithm/string.hpp"
+#include "boost/array.hpp"
+#include "boost/noncopyable.hpp"
+#include "boost/enable_shared_from_this.hpp"
+#include "boost/optional.hpp"
+#include "boost/shared_ptr.hpp"
+#include "boost/program_options.hpp"
+#include "boost/log/core.hpp"
+#include "boost/chrono.hpp"
+#include "boost/algorithm/string/trim.hpp"
+#include "boost/log/attributes/current_process_name.hpp"
+#include "boost/log/expressions.hpp"
+#include "boost/log/sinks.hpp"
+#include "boost/log/support/date_time.hpp"
+#include "boost/log/utility/setup/common_attributes.hpp"
+#include "boost/log/utility/setup/console.hpp"
+#include "boost/log/trivial.hpp"*/
+
+/*#pragma warning(default:4512)
+#pragma warning(default:4996)
+#pragma warning(default:4510  )
+#pragma warning(default:4610)
+#pragma warning(default:4100)*/
+#endif
